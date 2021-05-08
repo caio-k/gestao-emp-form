d895a12105116c146a0ff81f71081a84b6f4c27f
Mauricio Tocura <mryutoc@gmail.com>
2021-05-08 13:19:59 -0300

    Adjust App and index to the new structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Trabalho de Gestão Empresarial</p>
-      </header>
-    </div>
-  );
-}
+import GlobalStyle from './styles/GlobalStyle'
+import Routes from './routes'
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Routes />
+  </>
+)
 
 export default App;
