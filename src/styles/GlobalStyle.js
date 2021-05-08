11664eda0bbb6989eb5a8d397f9dd01256986a10
Mauricio Tocura <mryutoc@gmail.com>
2021-05-08 11:15:20 -0300

    Adjust the Global Style

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,51 +1,49 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: ${props => props.theme.fonts.family.montserrat};
-    background: ${props => props.theme.colors.background};
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    font-size: 15px;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0 !important;
+    font-family: 'Montserrat', 'Roboto', sans-serif;;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
   }
-  #root {
-    color: ${props => props.theme.colors.gray400};
-    background: ${props => props.theme.colors.background};
-    font-family: ${props => props.theme.fonts.family.montserrat};
-    p {
-      font-size: 1rem;
-      line-height: 1.86rem;
-      margin: 0.8125rem 0;
-      padding: 0;
-      font-weight: 600;
-    }
-    h1, h2, h3, h4, h5, h6 {
-      font-family: ${props => props.theme.fonts.family.montserrat};
-    }
-    h1 {
-      font-size: 2.5rem;
-      font-weight: 200;
-    }
-    h2 {
-      font-size: 2rem;
-      font-weight: 300;
-    }
-    h3 {
-      font-size: 1.75rem;
-      font-weight: 300;
-    }
-    h4 {
-      font-size: 1.5rem;
-      font-weight: medium;
-    }
-    h5 {
-      font-size: 1.25rem;
-      font-weight: 600;
-    }
-    h6 {
-      font-size: 1rem;
-      font-weight: bold;
-    }
+
+  body {
+    background: #f5f7f9;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px Roboto, ans-serif;
+  }
+
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+    min-height: 100%;
+  }
+
+  a {
+    text-decoration: none !important;
+  }
+
+  .clearfix {
+    clear: both;
+  }
+
+  .noselect {
+    -webkit-touch-callout: none; /* iOS Safari */
+      -webkit-user-select: none; /* Safari */
+      -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Firefox */
+          -ms-user-select: none; /* Internet Explorer/Edge */
+              user-select: none; /* Non-prefixed version, currently
+                                    supported by Chrome and Opera */
   }
 `;
 
